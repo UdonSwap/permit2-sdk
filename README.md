af761a5a46f5d7ad18d9577f982242f996c4240e
darshitbhalodialampros <darshitbhalodia@lampros.tech>
2024-06-12 12:09:06 +0530

    new version code

---
+++ README.md
@@ -0,0 +1,22 @@
+# permit2-sdk
+
+## Local Development
+
+### Checkout the repo
+
+- `git clone --recurse-submodules https://github.com/UdonSwap/permit2-sdk.git`
+
+### Install dependencies
+
+- `yarn`
+
+### Run Unit Tests
+
+- `yarn test`
+
+### Run Forge tests
+
+- `yarn build`
+- `yarn interop`
+- `(cd permit2 && forge build)`
+- `forge test`
---
+++ src/constants.ts
@@ -0,0 +1,20 @@
+import { BigNumber } from '@ethersproject/bignumber'
+
+export const PERMIT2_ADDRESS = '0xb36E4b925745E07D53F22a6F7e281fefEB40bCd6'
+
+export const MaxUint48 = BigNumber.from('0xffffffffffff')
+export const MaxUint160 = BigNumber.from('0xffffffffffffffffffffffffffffffffffffffff')
+export const MaxUint256 = BigNumber.from('0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
+
+// alias max types for their usages
+// allowance transfer types
+export const MaxAllowanceTransferAmount = MaxUint160
+export const MaxAllowanceExpiration = MaxUint48
+export const MaxOrderedNonce = MaxUint48
+
+// signature transfer types
+export const MaxSignatureTransferAmount = MaxUint256
+export const MaxUnorderedNonce = MaxUint256
+export const MaxSigDeadline = MaxUint256
+
+export const InstantExpiration: BigNumber = BigNumber.from(0)
